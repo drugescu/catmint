624e193c03ed77bbef25286d7e4b8e68bdaf2fc0
drugescu <dragos.rugescu@yahoo.com>
2019-12-03 20:40:53 +0200

    Testing framework developed further.

--- catmint-lex/wtest.sh
+++ catmint-lex/wtest.sh
@@ -1,28 +1,55 @@
 #!/bin/sh
+
+GREEN='\033[1;32m'
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+WHITE='\033[1;37m'
+NC='\033[0m'
+
 errors=0
 tests=0
+
+printf "\n\n"
+echo "${WHITE}Running the Catmint parser test suite...${NC}"
+echo -n "Removing all AST files from test_suite..."
 rm -f ./test_suite/*.ast
+rm -f ./test_suite/*.errlog.txt
+echo " done."
+echo "Iterating through tests..."
+echo "----------------------------------------------------------------------"
 for file in ./test_suite/*.cm;
 do
   tests=$((tests+1))
-  echo "Running test file << $file >>"
+  echo "Running test file << ${YELLOW}$file${NC} >>"
   ./bin/catmint-parser $file $file.ast
   echo "Parsed << $file >>"
-  DIFF=$(diff $file.ast $file.ref)
-  if [ "$DIFF" ]
-  then
-    echo "[ ERROR ] : unexpected AST output."
+  
+  # Test if reference file exists
+  if [ -f "$file.ref" ]; then
+  
+    DIFF=$(diff $file.ast $file.ref)
+    if [ "$DIFF" ]
+    then
+      echo "[ ${RED}ERROR${NC} ] : unexpected AST output! Consult << $file.errlog.txt >>"
+      errors=$((errors+1))
+      echo "$DIFF" > $file.errlog.txt
+    else
+      printf "[ ${GREEN}OK${NC} ] Output matches reference output.\n"
+    fi
+  
+  else
+    echo "[ [ ${RED}ERROR${NC} ] ] : reference AST output does not exist!"
     errors=$((errors+1))
   fi
-  #cat $file.ast
-  echo "-----------------------------------------------------------------"
+  # cat $file.ast
+  echo "----------------------------------------------------------------------"
 done
 
 if [ $errors -ne 0 ]
 then
-  echo "$errors/$tests tests have failed!"
+  echo "[ ${RED}ERROR${NC} ] : $errors/$tests tests have failed!"
 else
-  echo "All tests OK."
+  echo "[ ${GREEN}OK${NC} ] all tests passed."
 fi
 
 #rm -f ./test/hellox.cm.ast
